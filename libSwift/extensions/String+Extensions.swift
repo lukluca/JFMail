a8a29af480331eccc061b669b976b927919c6565
Luca Tagliabue <l.tagliabue@accenture.com>
2017-05-30 20:13:00 +0200

    Created String+Extensions

---
+++ libSwift/extensions/String+Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  String+Extensions.swift
+//  JFMail
+//
+//  Created by Tagliabue, L. on 29/05/2017.
+//  Copyright © 2017 jeffsss. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    
+    init?(bytes: UnsafeMutableRawPointer, length: Int, encoding: String.Encoding){
+        self.init(bytesNoCopy: bytes, length: length, encoding: encoding, freeWhenDone: true)
+    }
+    
+    
+}
